b75066a4bb77a32cc1cdfe082e89a5999d224614
KBroJ <kbroj9210@gmail.com>
2022-03-10 23:41:25 +0900

    MyPageController.java_어노테이션 추가(slf4j, sessionAttributes(l"oginMebmer"))

--- bebeno/src/main/java/com/bebeno/mvc/mypage/controller/MyPageController.java
+++ bebeno/src/main/java/com/bebeno/mvc/mypage/controller/MyPageController.java
@@ -3,8 +3,13 @@ package com.bebeno.mvc.mypage.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
+@SessionAttributes("loginMember")
 @RequestMapping("/mypage")
 public class MyPageController {
 	
