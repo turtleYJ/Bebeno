2ce884f3e13fa088f868e613450a5bb60e3ccf7a
KBroJ <kbroj9210@gmail.com>
2022-03-10 16:20:55 +0900

    오류찾기 1. mypage/model_service/dao/vo 붙여넣기 => 정상실행                 2. myPage-mapper.xml 붙여넣기 => 정상 실행
    MyPageController.java 코드에서 오류가 나는걸로 추측 ...

---
+++ bebeno/src/main/java/com/bebeno/mvc/mypage/model/service/MyPageService.java
@@ -0,0 +1,12 @@
+package com.bebeno.mvc.mypage.model.service;
+
+import com.bebeno.mvc.mypage.model.vo.MyPage;
+
+public interface MyPageService {
+
+	MyPage getMemberById(String id);
+
+	// 비밀번호 변경
+	int modifyPwd(String id, String newPwd);
+
+}
---
+++ bebeno/src/main/java/com/bebeno/mvc/mypage/model/service/MyPageServiceImpl.java
@@ -0,0 +1,47 @@
+package com.bebeno.mvc.mypage.model.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bebeno.mvc.member.model.vo.Member;
+import com.bebeno.mvc.mypage.model.dao.MyPageMapper;
+import com.bebeno.mvc.mypage.model.vo.MyPage;
+
+@Service
+public class MyPageServiceImpl implements MyPageService {
+
+	@Autowired
+	private MyPageMapper mapper;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
+// ==================================================================
+	
+	// id값으로 회원정보 조회하기
+	@Override
+	public MyPage getMemberById(String id) {		
+		
+		return mapper.getMemberById(id);
+	}
+
+// ========================= 비밀번호 변경 ==============================
+	@Override
+	@Transactional
+	public int modifyPwd(String id, String newPwd) {
+		
+		int result = 0;
+		
+		String encodeNewPwd = null;
+		
+		encodeNewPwd = passwordEncoder.encode(newPwd); // 비밀번호 암호화
+				
+//		result = mapper.modifyPwd(id, newPwd);
+		result = mapper.modifyPwd(id, encodeNewPwd);
+		
+		return result;
+	}
+	
+}
