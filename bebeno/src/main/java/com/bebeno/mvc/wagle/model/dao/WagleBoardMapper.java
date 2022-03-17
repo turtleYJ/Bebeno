8c3ee09b3b7045aad40ff1d9e07c27778a5afd22
haeun <gohe0301@gmail.com>
2022-03-18 07:49:21 +0900

    220318 wagleBoard 관련 재커밋
    - 뭔가 잘못 눌러서 다시 커밋합니다..

--- bebeno/src/main/java/com/bebeno/mvc/wagle/model/dao/WagleBoardMapper.java
+++ bebeno/src/main/java/com/bebeno/mvc/wagle/model/dao/WagleBoardMapper.java
@@ -5,10 +5,20 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.bebeno.mvc.wagle.model.vo.Wagle;
+import com.bebeno.mvc.wagle.model.vo.WagleFile;
 
 @Mapper
 public interface WagleBoardMapper {
 
 	List<Wagle> getWagleList();
+	
+	Wagle selectBoardByNo(int no);
+	
+	List<WagleFile> selectfilesByNo(int no);
+
+	int updateWagleBoard(Wagle wagleboard);
+
+	
+	
 
 }
--- bebeno/src/main/java/com/bebeno/mvc/wagle/model/service/WagleBoardService.java
+++ bebeno/src/main/java/com/bebeno/mvc/wagle/model/service/WagleBoardService.java
@@ -3,9 +3,18 @@ package com.bebeno.mvc.wagle.model.service;
 import java.util.List;
 
 import com.bebeno.mvc.wagle.model.vo.Wagle;
+import com.bebeno.mvc.wagle.model.vo.WagleFile;
 
 public interface WagleBoardService {
 
 	List<Wagle> getWagleList();
+	
+	Wagle findBoardByNo(int no);
+
+	List<WagleFile> findfileByNo(int no);
+	
+	Wagle delete(long no);
+
+	Wagle save(Wagle wagleboard);
 
 }
